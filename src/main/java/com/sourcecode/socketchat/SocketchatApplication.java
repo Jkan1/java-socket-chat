5f2f7ecbeae371e7aa677f88a3af008f0e446de3
JKan1 <kan.joshi.1@gmail.com>
2020-09-08 14:36:58 +0530

    Initial Commit

---
+++ src/main/java/com/sourcecode/socketchat/SocketchatApplication.java
@@ -0,0 +1,13 @@
+package com.sourcecode.socketchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketchatApplication.class, args);
+	}
+
+}
